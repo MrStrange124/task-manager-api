a124a7fd2e0ee13e8484fe417b65ca6d198d51cd
Aditya <ars124c@gmail.com>
2021-06-22 23:14:54 +0530

    init commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,21 @@
+const log = console.log
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
+
+const Task = mongoose.model('Task', {
+  description: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  }
+})
---
+++ src/models/task.js
@@ -0,0 +1,25 @@
+// const validator = require('validator')
+const mongoose = require('mongoose')
+
+const TaskSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  }
+}, {
+  timestamps: true
+})
+
+const tasks = mongoose.model('task', TaskSchema)
+
+module.exports = tasks
